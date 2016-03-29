4d16f2d752e7c700dcbbfc14975aa667c75c0116
Edy Suardiyana <putrai@uni.coventry.ac.uk>
2016-03-29 17:21:13 +1100

    slight changes on main function

--- main.py
+++ main.py
@@ -27,13 +27,13 @@ def main():
         source_weka = source_var.source_path_wekafile(name)
         source_runtime = source_var.source_runtime(name)
 
-        scale_file.scale_file(source_raw_data, source_file_micro, indexes[0], indexes[1])
-        #microannotate_right.micro_annotate(source_file_micro, micro_path) # re-annotate the raw data using micro-annotate
-        #cascade_with_rule.run_cascade(freq_rate,micro_path,features_path,source_runtime) # the latest cascade classifier
+        #scale_file.scale_file(source_raw_data, source_file_micro, indexes[0], indexes[1])
+        microannotate_right.micro_annotate(source_file_micro, micro_path) # re-annotate the raw data using micro-annotate
+        cascade_with_rule.run_cascade(freq_rate,micro_path,features_path,source_runtime) # the latest cascade classifier
         #non_cascade.run_feat_calc(name, freq_rate,micro_path,features_path) # this function is for non-cascade processing
-        #weka_file.write_weka(features_path, source_weka) # create the weka file
+        weka_file.write_weka(features_path, source_weka) # create the weka file
 
-    #mainTrainingGenerator.generate_training()
+    mainTrainingGenerator.generate_training()
 
 
 def read_subject_name(source_path):
